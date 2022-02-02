12013c7bbeebe7c3044581d6b8ea7dbda736a593
awatersny <awatersny@gmail.com>
2022-02-02 17:56:07 -0500

    Add functionality to event listener

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
+// Create cached element references for each element you created above with an id.
 const textBox = document.getElementById('text-box');
 const todoList = document.getElementById('todo-list');
 const submitButton = document.getElementById('submit-button');
-// 2. Add an event listener to the button that listens to click events. When the button is clicked, we need to:
+
 const addLi = evt => {
   //     - Create a new `li` element.
+  const newLi = document.createElement('li');
   //     - Set the text of the newly created `li` element to the text that the user has entered into the `input` element.
+  newLi.textContent = textBox.value;
   //     - Reset the text in the `input` field to an empty string.
+  textBox.value = '';
   //     - Add the new `li` element to the page, inside the `ul` element.
-  console.log(evt);
+  todoList.appendChild(newLi);
 }
-
+// Add an event listener to the button that listens to click events. When the button is clicked, we need to:
 submitButton.addEventListener('click', addLi);
